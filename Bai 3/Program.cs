fd439518216c034c9a73c087cfe84b1343d3e9d7
Yasuo <vudeptrai@hacker.com>
2020-05-26 18:16:51 +0700

    Creat Project

---
+++ Bai 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.Write("nhap toa do x : ");
+            x = Int32.Parse(Console.ReadLine());
+            Console.Write("nhap toa do y : ");
+            y = Int32.Parse(Console.ReadLine());
+            if (x == 0 && y == 0) { Console.WriteLine(" diem nam o goc toa do"); }
+            if (x != 0 && y == 0) { Console.WriteLine(" diem do thuoc truc hoang"); }
+            if (x == 0 && y != 0) { Console.WriteLine(" diem do thuoc truc tung"); }
+            if (x > 0 && y > 0) { Console.WriteLine(" diem do thuoc goc Q1"); }
+            if (x < 0 && y > 0) { Console.WriteLine(" diem do thuoc goc Q2"); }
+            if (x < 0 && y < 0) { Console.WriteLine(" diem do thuoc goc Q3"); }
+            if (x > 0 && y < 0) { Console.WriteLine(" diem do thuoc goc Q4"); }
+        }
+    }
+}
